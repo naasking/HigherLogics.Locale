022adb8c8c70318d99e8794f2a0579fae293bcac
Sandro Magi <none@none>
2020-04-21 12:36:19 -0400

    use standard class name filled in missing parameter docs

--- HigherLogics.Locale/Locales.cs
+++ HigherLogics.Locale/Locales.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 namespace HigherLogics.Locale
 {
+    /// <summary>
+    /// Local-specific information.
+    /// </summary>
     public static partial class Locales
     {
         /// <summary>
@@ -18,24 +21,24 @@ namespace HigherLogics.Locale
         /// Lookup the official state's name.
         /// </summary>
         /// <param name="country">The country to which this state belongs.</param>
-        /// <param name="stateName"></param>
-        /// <returns></returns>
+        /// <param name="stateName">The possibly unofficial state name.</param>
+        /// <returns>The official state name.</returns>
         public static string State(this Country country, string stateName) =>
             provinces[country][stateName];
 
         /// <summary>
         /// Look up the country the official currency.
         /// </summary>
-        /// <param name="currency"></param>
-        /// <returns></returns>
+        /// <param name="currency">The currency.</param>
+        /// <returns>The list of countries that use the given currency.</returns>
         public static IEnumerable<Country> Countries(this Currency currency) =>
             currency2Country[(int)currency];
 
         /// <summary>
         /// Lookup the official currencies used by a country.
         /// </summary>
-        /// <param name="country"></param>
-        /// <returns></returns>
+        /// <param name="country">The country.</param>
+        /// <returns>The currency for the given country, if available.</returns>
         public static Currency? Currency(this Country country) =>
             country2Currency.TryGetValue(country, out var c)
             ? c
