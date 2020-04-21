36620dde6b6cd1e055c22cdde0cc6049413941c7
Sandro Magi <none@none>
2020-04-21 14:11:38 -0400

    added a money type

---
+++ HigherLogics.Locale/Fixed.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HigherLogics
+{
+    /// <summary>
+    /// A fixed precision numerical type.
+    /// </summary>
+    /// <typeparam name="T">The units for this number.</typeparam>
+    readonly struct Fixed<T> : IEquatable<Fixed<T>>, IComparable<Fixed<T>>
+        where T : struct
+    {
+        public Fixed(decimal value, T units)
+        {
+            Value = value;
+            Units = units;
+        }
+
+        public decimal Value { get; }
+
+        public T Units { get; }
+
+        public bool Equals(Fixed<T> other) =>
+            Value == other.Value && EqualityComparer<T>.Default.Equals(Units, other.Units);
+
+        public int CompareTo(Fixed<T> other)
+        {
+            var x = Comparer<T>.Default.Compare(Units, other.Units);
+            return x == 0 ? Value.CompareTo(other.Value) : x;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override bool Equals(object obj) =>
+            obj is Fixed<T> f && Equals(f);
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override int GetHashCode() =>
+            Value.GetHashCode();
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override string ToString() =>
+            Value.ToString();
+
+        public static Fixed<T> operator /(Fixed<T> value, decimal constant) =>
+            new Fixed<T>(value.Value / constant, value.Units);
+
+        public static Fixed<T> operator *(Fixed<T> value, decimal constant) =>
+            new Fixed<T>(value.Value * constant, value.Units);
+
+        public static Fixed<T> operator *(decimal constant, Fixed<T> value) =>
+            value * constant;
+
+        public static Fixed<T> operator *(Fixed<T> lhs, Fixed<T> rhs) =>
+            lhs * rhs.Value;
+
+        public static Fixed<T> operator +(Fixed<T> lhs, Fixed<T> rhs) =>
+            new Fixed<T>(lhs.Value + rhs.Value, lhs.Units);
+        
+        public static Fixed<T> operator -(Fixed<T> lhs, Fixed<T> rhs) =>
+            new Fixed<T>(lhs.Value - rhs.Value, lhs.Units);
+    }
+}
--- HigherLogics.Locale/Money.cs
+++ HigherLogics.Locale/Money.cs
@@ -8,39 +8,27 @@ namespace HigherLogics.Locale
     /// <summary>
     /// A monetary value.
     /// </summary>
-    /// <typeparam name="Currency">The kind of the currency type.</typeparam>
     /// <remarks>
     /// Inspired by: https://deque.blog/2017/08/17/a-study-of-4-money-class-designs-featuring-martin-fowler-kent-beck-and-ward-cunningham-implementations/
     /// </remarks>
     public readonly struct Money : IComparable<Money>, IEquatable<Money>
     {
-        // when sum == null, amount and currency are valid.
-        // when sum != null, amount and currency are invalid.
-        readonly decimal amount;
-        readonly Currency currency;
-        readonly List<Money> sum;
+        // this is a sorted map of numbers indexed by currency, in monototonically
+        // increasing order, like a binary tree
+        readonly Fixed<Currency>[] values;
 
         /// <summary>
         /// Construct a simple monetary value.
         /// </summary>
         /// <param name="amount"></param>
         /// <param name="currency"></param>
-        public Money(decimal amount, Currency currency)
+        public Money(decimal amount, Currency currency) : this(new[] { new Fixed<Currency>(amount, currency) })
         {
-            this.amount = amount;
-            this.currency = currency;
-            this.sum = null;
         }
-        
-        Money(List<Money> sum)
-        {
-            Debug.Assert(sum.All(x => x.sum == null));
 
-            // ensure all monetary values are ordered consistently so equality works
-            sum.Sort();
-            this.sum = sum;
-            this.amount = 0;
-            this.currency = default(Currency);
+        Money(params Fixed<Currency>[] values)
+        {
+            this.values = values;
         }
 
         /// <summary>
@@ -48,141 +36,130 @@ namespace HigherLogics.Locale
         /// </summary>
         /// <param name="other"></param>
         /// <returns></returns>
-        public bool Equals(Money other)
-        {
-            // monetary values must either be:
-            // 1. simple values with equal amounts and equal currencies
-            // 2. sums with the same sequence of sub-values, in the same order
-            return sum == null && other.sum == null && amount == other.amount && currency.CompareTo(other.currency) == 0
-                || sum != null && other.sum != null && PairwiseEqual(sum, other.sum);
-        }
-
-        static bool PairwiseEqual(List<Money> left, List<Money> right)
-        {
-            if (left.Count != right.Count)
-                return false;
-            for (int i = 0; i < left.Count; ++i)
-                if (!left[i].Equals(right[i]))
-                    return false;
-            return true;
-        }
-
-        static int PairwiseCompare(List<Money> left, List<Money> right)
-        {
-            for (int i = 0; i < Math.Min(left.Count, right.Count); ++i)
-            {
-                var x = left[i].CompareTo(right[i]);
-                if (x != 0)
-                    return x;
-            }
-            return 0;
-        }
+        public bool Equals(Money other) =>
+            values.SequenceEqual(other.values);
 
         /// <summary>
         /// Compare two monetary values.
         /// </summary>
         /// <param name="other"></param>
         /// <returns></returns>
-        public int CompareTo(Money other)
+        public int CompareTo(Money other) =>
+            values.Zip(other.values, ValueTuple.Create).Select(x => x.Item1.CompareTo(x.Item2)).First(x => x != 0);
+
+        public static Money operator +(Money lhs, Money rhs) =>
+            new Money(Add(lhs.values, rhs.values));
+
+        public static Money operator -(Money lhs, Money rhs) =>
+            new Money(Subtract(lhs.values, rhs.values));
+
+        public static Money operator *(Money lhs, decimal rhs) =>
+            new Money(Multiply(lhs.values, rhs));
+
+        public static Money operator *(decimal lhs, Money rhs) =>
+            new Money(Multiply(rhs.values, lhs));
+
+        public static Money operator /(Money lhs, decimal constant) =>
+            new Money(Divide(lhs.values, constant));
+
+        public static Money operator -(Money money) =>
+            -1 * money;
+
+        #region Internal array ops
+        static T[] Insert<T>(T[] source, int i, T item)
         {
-            if (sum == null && other.sum == null)
+            var arr = new T[source.Length + 1];
+            Array.Copy(source, 0, arr, 0, i);
+            arr[i] = item;
+            Array.Copy(source, 0, arr, i + 1, source.Length - i);
+            return arr;
+        }
+
+        static int Missing(Fixed<Currency>[] lhs, Fixed<Currency>[] rhs)
+        {
+            //FIXME: could just do left.Length + right.Length for simplicity and efficiency.
+            //Or maybe, could use a heuristic based on the number of currency values because
+            //those are always fixed at runtime
+            // ie. scale factor = lhs.length / count(Currency) + rhs.Length/count(Currency)
+
+            // find out how many entries are in one but not the other array
+            var shorter = lhs.Length < rhs.Length ? lhs : rhs;
+            var longer = shorter == lhs ? rhs : lhs;
+            var missing = 0;
+            for (int i = 0, j = 0; i < shorter.Length; ++i)
             {
-                var x = currency.CompareTo(other.currency);
-                return x == 0 ? amount.CompareTo(other.amount) : x;
+                var x = shorter[i];
+                j = Array.BinarySearch(longer, j, longer.Length, x);
+                if (j < 0)
+                {
+                    ++missing;
+                    j = ~j;
+                }
             }
-            else if (other.sum == null)
+            return missing;
+        }
+
+        static Fixed<Currency>[] Add(Fixed<Currency>[] lhs, Fixed<Currency>[] rhs)
+        {
+            // given an array of appropriate size, traverse all arrays and copy the items in-order
+            // so as to preserve the binary search invariant of monotonically increasing values
+            var arr = new Fixed<Currency>[Missing(lhs, rhs) + lhs.Length];
+            for (int i = 0, s = 0, l = 0; i < arr.Length; ++i)
             {
-                return 1; // simple values should appear before sums
+                switch (lhs[s].CompareTo(rhs[l]))
+                {
+                    case -1:
+                        arr[i] = lhs[s++];
+                        break;
+                    case 0:
+                        arr[i] = lhs[s++] + rhs[l++];
+                        break;
+                    case 1:
+                        arr[i] = rhs[l++];
+                        break;
+                }
             }
-            else
+            return arr;
+        }
+
+        static Fixed<Currency>[] Subtract(Fixed<Currency>[] lhs, Fixed<Currency>[] rhs)
+        {
+            // given an array of appropriate size, traverse all arrays and copy the items in-order
+            // so as to preserve the binary search invariant of monotonically increasing values
+            var arr = new Fixed<Currency>[Missing(lhs, rhs) + lhs.Length];
+            for (int i = 0, l = 0, r = 0; i < arr.Length; ++i)
             {
-                // generates a lazy sequence of (0 | positive | negative), so we just return the first non-zero
-                // if no non-zero, this returns 0 anyway, which indicates equality
-                return PairwiseCompare(sum, other.sum);
+                switch (lhs[l].CompareTo(rhs[r]))
+                {
+                    case -1:
+                        arr[i] = lhs[l++];
+                        break;
+                    case 0:
+                        arr[i] = lhs[l++] - rhs[r++];
+                        break;
+                    case 1:
+                        arr[i] = rhs[r++];
+                        break;
+                }
             }
+            return arr;
         }
 
-        /// <summary>
-        /// Return the decimal amount for the given currency.
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="xchg"></param>
-        /// <returns></returns>
-        public decimal Amount(Currency target, Func<Currency, Currency, decimal> xchg)
+        static Fixed<Currency>[] Multiply(Fixed<Currency>[] values, decimal constant)
         {
-            if (xchg == null)
-                throw new ArgumentNullException("xchg");
-            return sum == null ? xchg(currency, target) * amount:
-                                 sum.Sum(x => x.Amount(target, xchg));
+            var arr = new Fixed<Currency>[values.Length];
+            for (int i = 0; i < arr.Length; ++i)
+                arr[i] = values[i] * constant;
+            return arr;
         }
 
-        /// <summary>
-        /// Multiply the monetary value by a constant.
-        /// </summary>
-        /// <param name="money"></param>
-        /// <param name="constant"></param>
-        /// <returns></returns>
-        public static Money operator *(Money money, decimal constant)
+        static Fixed<Currency>[] Divide(Fixed<Currency>[] values, decimal constant)
         {
-            return money.sum == null
-                 ? new Money(money.amount * constant, money.currency)
-                 : new Money(money.sum.Select(x => x * constant).ToList());
+            var arr = new Fixed<Currency>[values.Length];
+            for (int i = 0; i < arr.Length; ++i)
+                arr[i] = values[i] / constant;
+            return arr;
         }
-
-        /// <summary>
-        /// Multiply the monetary value by a constant.
-        /// </summary>
-        /// <param name="money"></param>
-        /// <param name="constant"></param>
-        /// <returns></returns>
-        public static Money operator *(decimal constant, Money money)
-        {
-            return money * constant;
-        }
-        
-        /// <summary>
-        /// Add two monetary values.
-        /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
-        public static Money operator +(Money left, Money right)
-        {
-            // flatten any embedded sums to ensure top-level monetary expressions only contain flat arrays of values
-            return left.sum != null && right.sum != null ? new Money(left.sum.Concat(right.sum).ToList()):
-                   left.sum != null                      ? new Money(Append(left.sum, right)):
-                   right.sum != null                     ? new Money(Append(right.sum, left)):
-                                                           new Money(new List<Money> { left, right });
-        }
-
-        static List<T> Append<T>(List<T> source, T item)
-        {
-            var list = new List<T>(source);
-            list.Add(item);
-            return list;
-        }
-
-        /// <summary>
-        /// Add two monetary values.
-        /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
-        public static Money operator -(Money left, Money right)
-        {
-            return left + -right;
-        }
-
-        /// <summary>
-        /// Negate a monetary value.
-        /// </summary>
-        /// <param name="money"></param>
-        /// <returns></returns>
-        public static Money operator -(Money money)
-        {
-            return money.sum == null
-                 ? new Money(-money.amount, money.currency)
-                 : new Money(money.sum.Select(x => -x).ToList());
-        }
+        #endregion
     }
 }
